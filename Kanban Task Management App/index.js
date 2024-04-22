766ca0e4fa66503614c140456086194d6596874a
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-04-22 17:18:03 +0200

    Fixed the bug for getting task details in the modal inputs

--- Kanban Task Management App/index.js
+++ Kanban Task Management App/index.js
@@ -290,9 +290,9 @@ function openEditTaskModal(task) {
   
 
   // Get button elements from the task modal
-  elements.editTaskTitleInput.value = title;
-  elements.editSelectStatus.value = status;
-  elements.editTaskDescInput.value = description;
+  elements.editTaskTitleInput.value = task.title;
+  elements.editSelectStatus.value = task.status;
+  elements.editTaskDescInput.value = task.description;
 
   // Call saveTaskChanges upon click of Save Changes button
   elements.saveTaskChangesBtn.onclick = () => saveTaskChanges(task);
