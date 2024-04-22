c448c96e2e8570d400af2762d87c610228979ac5
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-04-22 14:01:43 +0200

    Imported helper functions from utils

--- Kanban Task Management App/index.js
+++ Kanban Task Management App/index.js
@@ -1,6 +1,11 @@
 // TASK: import helper functions from utils
 // TASK: import initialData
-
+import {
+  getTasks,
+  createNewTask,
+  patchTask,
+  putTask,
+  deleteTask} from './utils/taskFunctions.js';
 
 /*************************************************************************************************************************************************
  * FIX BUGS!!!
